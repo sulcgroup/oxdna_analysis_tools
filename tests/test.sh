93983f171430736e5e33706ec7f124311245cc15
ErikPoppleton <epopplet@asu.edu>
2020-03-09 17:50:30 -0700

    added tests to make sure things don't crash

---
+++ tests/test.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+echo "Testing align_trajectory.py..."
+if
+    ../align_trajectory.py minitraj.dat rna_tile.top aligned.dat 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing bond_analysis.py..."
+if
+    ../bond_analysis.py input_rna minitraj.dat pairs.txt pairs.json 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing compute_mean.py and compute_deviations.py..."
+if
+    ../compute_mean.py -d devs.json minitraj.dat rna_tile.top 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing centroid.py..."
+if
+    ../centroid.py -i index.txt mean.dat minitraj.dat rna_tile.top 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing distance.py and clustering.py (this one takes a while because of the plot)..."
+if
+    ../distance.py -c -i input_rna minitraj.dat 1 3 5 67 34 56 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing duplex_angle_finder.py..."
+if
+    ../duplex_angle_finder.py input_rna minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing duplex_angle_plotter.py..."
+if
+    ../duplex_angle_plotter.py -o angle.png -i angles.txt 7 37 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing multidimensional_scaling_mean.py..."
+if
+    ../multidimensional_scaling_mean.py input_rna minitraj.dat meanM devsM 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing output_bonds.py..."
+if
+    ../output_bonds.py -v all_energy.json input_rna minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
+
+echo ""
+
+echo "Testing pca.py"
+if
+    ../pca.py input_rna minitraj.dat mean.dat pca.json 2>&1 >/dev/null | grep -y "ERROR"
+then
+    echo "AN ERROR OCCURED"
+else
+    echo "OK"
+fi
