de92aa465761b69b567d3d6871eb40d50852441a
ErikPoppleton <rkpoppleton@gmail.com>
2021-03-23 17:17:50 -0700

    added reference option to align_trajectory

--- align_trajectory.py
+++ align_trajectory.py
@@ -15,11 +15,30 @@ import numpy as np
 import argparse
 from compute_mean import normalize
 
+#aligner
+def align_frame(ref_conf, sup, mysystem):
+    #Need to get rid of fix_diffusion artifacts or SVD doesn't work
+    mysystem.inbox()
+    indexed_cur_conf = mysystem.positions[indexes]
+
+    #Superimpose the configuration to the reference
+    sup.set(ref_conf, indexed_cur_conf)
+    sup.run()
+    rot, tran = sup.get_rotran()
+
+    #Apply rotation and translation in one step
+    mysystem.positions = np.einsum('ij, ki -> kj', rot, mysystem.positions) + tran
+    mysystem.a1s = np.einsum('ij, ki -> kj', rot, mysystem.a1s)
+    mysystem.a3s = np.einsum('ij, ki -> kj', rot, mysystem.a3s)
+
+    return mysystem
+
 #handle commandline arguments
 parser = argparse.ArgumentParser(description="Aligns each frame in a trajectory to the first frame")
 parser.add_argument('traj', type=str, nargs=1, help="The trajectory file to align")
 parser.add_argument('outfile', type=str, nargs=1, help='The name of the new trajectory file to write out')
 parser.add_argument('-i', metavar='index_file', dest='index_file', nargs=1, help='Align to only a subset of particles from a space-separated list in the provided file')
+parser.add_argument('-r', metavar='reference_structure', dest='reference_structure', nargs=1, help="Align to a provided configuration instead of the first frame.")
 args = parser.parse_args()
 
 #run system checks
@@ -29,6 +48,7 @@ check_dependencies(["python", "numpy", "Bio"])
 #Parse command line arguments
 traj_file = args.traj[0]
 outfile = args.outfile[0]
+sup = SVDSuperimposer()
 
 #-i will make it only run on a subset of nucleotides.
 #The index file is a space-separated list of particle IDs
@@ -44,35 +64,36 @@ else:
     with ErikReader(traj_file) as r:
         indexes = list(range(len(r.read().positions)))
 
-#read the first configuration and use it as the reference configuration for the rest
-r = ErikReader(traj_file)
-ref = r.read()
-ref.inbox()
-ref_conf = ref.positions[indexes]
-sup = SVDSuperimposer()
+#-r will make it align to a provided .dat file instead of the first configuration
+if args.reference_structure:
+    #read reference configuration
+    r = ErikReader(args.reference_structure[0])
+    ref = r.read()
+    ref.inbox()
+    r = ErikReader(traj_file)
+    ref_conf = ref.positions[indexes]
+    
+    mysystem = align_frame(ref_conf, sup, r.read())
 
-#The topology remains the same so we only write the configuration
-ref.write_new(outfile)
+else:
+    #read the first configuration and use it as the reference configuration for the rest
+    r = ErikReader(traj_file)
+    mysystem = r.read()
+    mysystem.inbox()
+    ref_conf = mysystem.positions[indexes]
+    
+#write first configuration to output file
+mysystem.write_new(outfile)
 mysystem = r.read()
+
+
 
 #Read the trajectory one configuration at a time and perform the alignment
 while mysystem != False:
     print("working on t = ", mysystem.time)
-    #Need to get rid of fix_diffusion artifacts or SVD doesn't work
-    mysystem.inbox()
-    indexed_cur_conf = mysystem.positions[indexes]
+    
+    mysystem = align_frame(ref_conf, sup, mysystem)
 
-    #Superimpose the configuration to the reference
-    sup.set(ref_conf, indexed_cur_conf)
-    sup.run()
-    rot, tran = sup.get_rotran()
-
-    #Apply rotation and translation in one step
-    mysystem.positions = np.einsum('ij, ki -> kj', rot, mysystem.positions) + tran
-    mysystem.a1s = np.einsum('ij, ki -> kj', rot, mysystem.a1s)
-    mysystem.a3s = np.einsum('ij, ki -> kj', rot, mysystem.a3s)
-
-    #print_lorenzo_output will create a new file, print_traj_output extends an existing file
     mysystem.write_append(outfile)
 
     mysystem = r.read()
