a6a6af9da5bc1c73c72fb2a5be5f7096d18ab7bb
zoombya <progolab@gmail.com>
2021-05-28 11:53:57 -0700

    optional skip on a and rounding of p

--- minify.py
+++ minify.py
@@ -1,26 +1,38 @@
 from UTILS.readers import ErikReader, cal_confs
-from sys import argv, exit
+import argparse
 from os import remove
-
-if len(argv)<3:
-	print("Discards velocity information from the given trajectory.")
-	print("Usage is >minify.py traj_file out_file")
-	exit()
-
-# input files
-traj_file,out = argv[1],argv[2]
-
-# make sure there is no outfile
-try:
-	remove(out)
-except:
-	pass
-# get the number of configurations
-n_confs = cal_confs(traj_file)
+from numpy import round
 
 
-with ErikReader(traj_file) as reader:
-	for i in range(n_confs):
-		print(i+1,":",n_confs)
-		system = reader.read()
-		system.write_append(out)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compress given configuration.")
+    parser.add_argument('trajectory', type=str, nargs=1, help='the trajectory file you wish to analyze')
+    parser.add_argument('outfile',    type=str, nargs=1, help='minified file')
+
+    parser.add_argument('-a', help='Discard a vectors.')
+    parser.add_argument('-p', action = 'store_true',  help='Round positions to 7 digits.')
+
+    args = parser.parse_args()
+
+    traj_file = args.trajectory[0]
+    out = args.outfile[0]
+    # get the number of configurations
+    n_confs = cal_confs(traj_file)
+
+    try: # make sure there is no out file
+        remove(out)
+    except:
+        pass
+    
+    with ErikReader(traj_file) as reader:
+        for i in range(n_confs):
+            print(i+1,":",n_confs)
+            # Erik reader ignores velocities 
+            system = reader.read()
+            if args.p: # round positions
+                system.positions = round(system.positions, 7)
+            if args.a: # discard a vectors
+                system.a1s -= system.a1s 
+                system.a3s -= system.a3s
+            # output conf
+            system.write_append(out)
