38bf8ad90e21fee39ea9ae2dfe33208d6b917d4f
ErikPoppleton <rkpoppleton@gmail.com>
2021-10-25 16:45:14 -0700

    preparing for upload to PyPi

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 setup()
 
-print("All set up! All scripts in the package can now be run from the command line with\noat <script_name>\nimporting functions in your own scripts can be done with\nfrom oxDNA_analysis_tools.<file name> import <function>")
+print("\n\n################################################################################")
+print("\nAll set up! All scripts in the package can now be run from the command line with\n\n    oat <script_name>\n\nimporting functions in your own scripts can be done with\n\n    from oxDNA_analysis_tools.<file name> import <function>")
 
 print("")
 
-print("If you would like to enable autocomplete in Bash, either add\n source oat-completion.sh\n to your .bashrc or run \nsudo cp oat-completion.sh /etc/bash_completion.d/\nto make autocompletes available to all users.")
+print("If you would like to enable autocomplete in Bash, either add\n\n    source oat-completion.sh\n\nto your .bashrc or run \n\n    sudo cp oat-completion.sh /etc/bash_completion.d/\n\nto make autocompletes available to all users.")
