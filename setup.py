2987244b8febd5d469460b54ac1a1fcca5fb5684
ErikPoppleton <rkpoppleton@gmail.com>
2021-10-25 14:40:44 -0700

    added autocomplete setup

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
 from setuptools import setup, find_packages
 setup()
+
+print("All set up! All scripts in the package can now be run from the command line with\noat <script_name>\nimporting functions in your own scripts can be done with\nfrom oxDNA_analysis_tools.<file name> import <function>")
+
+print("")
+
+print("If you would like to enable autocomplete in Bash, either add\n source oat-completion.sh\n to your .bashrc or run \nsudo cp oat-completion.sh /etc/bash_completion.d/\nto make autocompletes available to all users.")
