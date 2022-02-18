553f97751ebd6d47bd7559b98e5f8ce6f86e11d5
ErikPoppleton <rkpoppleton@gmail.com>
2022-02-18 10:01:18 -0700

    merge Micha's updates to Elliot's code

--- src/oxDNA_analysis_tools/m_mean.py
+++ src/oxDNA_analysis_tools/m_mean.py
@@ -1,19 +1,20 @@
-from oxDNA_analysis_tools.UTILS.micha_reader import MichaReader
-from oxDNA_analysis_tools.UTILS.base_array import base_array
-try:
-    from Bio.SVDSuperimposer import SVDSuperimposer
-except:
-    from bio.SVDSuperimposer import SVDSuperimposer
-from oxDNA_analysis_tools.UTILS.micha_reader import MichaReader
 import numpy as np
 from os import getenv, path
-from oxDNA_analysis_tools.UTILS.micha_reader import partition, flatten, handle_confs,parse_conf
 from multiprocessing import cpu_count, Pool
-from functools import partial
-
+from collections import namedtuple
+from io import StringIO
+from pickle import loads, dumps
+from os.path import exists
+import os
 
 import time
 start_time = time.time()
+
+
+
+top, traj = "./hinge_correct_seq.top","./aligned.dat"
+
+
 
 #figure out how many chunks we are working on 
 
@@ -44,17 +45,58 @@ def align(centered_ref_coords, cms_ref_cords ,coords):
              np.dot(coords[:, 6:9], rot))
 
 
+<<<<<<< Updated upstream
 def compute(top, traj,centered_ref_coords, cms_ref_cords, ntopart, ptr):
     reader = MichaReader(top,traj)
     confs = reader._get_confs(ptr*ntopart,ntopart)
 
     # convert to numpy repr
     aligned_coords = reader._parse_confs(confs)
+=======
+BaseArray = namedtuple("BaseArray", ["time","box", "energy", "positions", "a1s", "a3s"])
+def parse_conf(lines,nbases):
+    lines = lines.split('\n')
+    parameters =  np.array([line.split(maxsplit=9)[0:9] for line in lines[3:3+nbases]],dtype=float)
+    conf = BaseArray(
+        float(lines[0][lines[0].index("=")+1:]),
+        np.array(lines[1].split("=")[1].split(), dtype=float),
+        np.array(lines[2].split("=")[1].split(), dtype=float),
+        parameters[:,0:3],
+        parameters[:,3:6],
+        parameters[:,6:9],
+    )
+    return conf
+
+def get_confs(idxs,traj_path, start, nconfs):
+    conf_count = len(idxs)
+    if(start+nconfs >= conf_count): # make sure we stay in bounds 
+        nconfs = conf_count - start
+    # go to start position 
+    with open(traj_path) as traj_file:
+        traj_file.seek(idxs[start].offset)
+        # figure out how big of a chunk we want to read 
+        size = sum([idxs[i].size for i in range(start,start+nconfs)])
+        # read chunk and prepare to split
+        chunk = StringIO(traj_file.read(size)) # work with the string like a file 
+        return [chunk.read(idxs[i].size)
+                            for i in range(start,start+nconfs)] 
+
+
+def compute(traj,nbases, idxs, centered_ref_coords, cms_ref_cords, ntopart, ptr):
+    #reader = MichaReader(top,traj)
+    confs = get_confs(idxs,traj, ptr*ntopart,ntopart)
+    #TODO: NEEDS INBOX
+    confs = [inbox(parse_conf(c,nbases)) for c in confs]
+    # convert to numpy repr
+    aligned_coords = np.asarray([[align_conf.positions, align_conf.a1s, align_conf.a3s] 
+                                                                  for align_conf in confs])
+>>>>>>> Stashed changes
     aligned_coords = [align(centered_ref_coords,cms_ref_cords, c) for c in aligned_coords]
     sub_mean = np.sum(aligned_coords, axis=0)
     return sub_mean
 
 
+<<<<<<< Updated upstream
 if __name__ == "__main__":
     top, traj = "/home/erik/Simulations/hinge_all/hinge1/hinge_correct_seq.top", "/home/erik/Simulations/hinge_all/hinge1/aligned.dat"
 
@@ -101,3 +143,150 @@ if __name__ == "__main__":
     ).write_new("/home/erik/Simulations/hinge_all/hinge1/mean.dat")
 
     print("--- %s seconds ---" % (time.time() - start_time))
+=======
+def write_conf(path,conf):
+    out = []
+    out.append('t = {}'.format(int(conf.time)))
+    out.append('b = {}'.format(' '.join(conf.box.astype(str))))
+    out.append('E = {}'.format(' '.join(conf.energy.astype(str))))
+    for p, a1, a3 in zip(conf.positions, conf.a1s, conf.a3s):
+        out.append('{} {} {} 0.0 0.0 0.0 0.0 0.0 0.0'.format(' '.join(p.astype(str)), ' '.join(a1.astype(str)), ' '.join(a3.astype(str))))
+    with open(path,"w") as f:
+        f.write("\n".join(out))
+
+
+
+
+
+# chunk file into blocks of given size
+Chunk = namedtuple('Chunk', ['block','offset', 'is_last','file_size'])
+def blocks(file, fsize, size=1000000):
+    current_chunk = 0  
+    while True:
+        b = file.read(size)
+        if not b: break
+        yield Chunk(b,current_chunk*size, current_chunk * size + size > fsize, fsize)
+        current_chunk+=1
+
+def find_all(a_str, sub):
+    #https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
+    start = 0
+    idxs = []
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return idxs
+        idxs.append(start)
+        start += len(sub) # use start += 1 to find overlapping matches
+
+#calculates the length of a trajectory file
+ConfInfo = namedtuple('ConfInfo', ['offset', 'size','id'])
+def index(traj_file): 
+    val = b"t" 
+    conf_starts = []
+    fsize = os.stat(traj_file).st_size 
+    for chunk in blocks(open(traj_file, 'rb'), fsize, size=1000000):#5500000):
+        idxs = np.array(find_all(chunk.block,val)) + chunk.offset # find all offsets of t in file
+        conf_starts.extend(idxs)
+
+    conf_starts = np.array(conf_starts)
+    #generate a list of confs for all but the last one
+    idxs = [ConfInfo(conf_starts[i], conf_starts[i+1] - conf_starts[i],i) 
+                                            for i in range(len(conf_starts)-1)]
+    #handle last offset
+    idxs.append(ConfInfo(conf_starts[-1], fsize - conf_starts[-1], len(conf_starts)-1))
+    return idxs
+
+
+def inbox(conf):
+    """
+    Modify the positions attribute such that all positions are inside the box.
+    """
+    def realMod (n, m):
+        return(((n % m) + m) % m)
+    def coord_in_box(p):
+        p = realMod(p, conf.box)
+        return(p)
+    def calc_PBC_COM(conf):
+        angle = (conf.positions * 2 * np.pi) / conf.box
+        cm = np.array([[np.sum(np.cos(angle[:,0])), np.sum(np.sin(angle[:,0]))], 
+        [np.sum(np.cos(angle[:,1])), np.sum(np.sin(angle[:,1]))], 
+        [np.sum(np.cos(angle[:,2])), np.sum(np.sin(angle[:,2]))]]) / len(angle)
+        return conf.box / (2 * np.pi) * (np.arctan2(-cm[:,1], -cm[:,0]) + np.pi)
+    target = np.array([conf.box[0] / 2, conf.box[1] / 2, conf.box[2] / 2])
+    center = calc_PBC_COM(conf)
+    positions = conf.positions + target - center 
+    
+    new_poses = coord_in_box(positions)
+    positions += (new_poses - conf.positions)
+    return BaseArray(
+        conf.time, conf.box, conf.energy,
+        positions, conf.a1s, conf.a3s
+    )
+
+
+#if idxs is None: # handle case when we have no indexes provided
+if not(exists(traj+".pyidx")):
+    idxs = index(traj) # no index created yet
+    with open(traj+".pyidx","wb") as file:
+        file.write(dumps(idxs)) # save it
+else:
+    #we can load the index file
+    with open(traj+".pyidx","rb") as file:
+        idxs = loads(file.read())
+
+
+
+with open(top) as f:
+    my_top_info = f.readline().split(' ')
+    if len(my_top_info)  == 2:
+        nbases, nstrands = my_top_info
+    elif len(my_top_info) == 5:
+        nbases, nstrands, ndna, nres, ndnastrands = my_top_info
+    nbases = int(nbases)
+
+ref_conf = parse_conf(
+    get_confs(idxs, traj, 0, 1)[0],
+    nbases
+)
+#TODO: NEEDS INBOX
+ref_conf = inbox(ref_conf)
+
+ncpus = get_n_cpu()
+ntopart = 50
+#prepare to fire multiple processes 
+pool = Pool(ncpus)
+
+n_confs  = len(idxs) 
+n_chunks = int(n_confs / ntopart +
+                     (1 if n_confs % ntopart else 0))
+
+reference_coords = ref_conf.positions
+av1 = np.mean(reference_coords, axis=0)
+reference_coords = reference_coords - av1
+
+
+
+print("starting up processes")
+results = [pool.apply_async(compute,(traj,len(ref_conf.positions), idxs,reference_coords, av1, ntopart,i)) 
+                        for i in range(n_chunks)]
+print("all spawned")
+
+
+# get the results
+acc = np.zeros([3, nbases, 3])
+for i,r in enumerate(results):
+    print(f"finished {i+1}/{n_chunks}",end="\r")
+    acc += r.get()
+print()
+# compute the mean 
+acc /= n_confs
+pos, a1s, a3s = acc
+
+# renormalize
+a1s = np.array([v/np.linalg.norm(v) for v in a1s])
+a3s = np.array([v/np.linalg.norm(v) for v in a3s])
+
+
+write_conf("./mean_m.dat",BaseArray(0,ref_conf.box,np.array([0,0,0]), pos, a1s , a3s))
+print("--- %s seconds ---" % (time.time() - start_time))
+>>>>>>> Stashed changes
