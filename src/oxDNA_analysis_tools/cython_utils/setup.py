9ba17f552e171b71d77e8751b00d61015394fb08
ErikPoppleton <rkpoppleton@gmail.com>
2022-03-28 06:53:34 -0700

    updated cythonize command in cython_utils

--- src/oxDNA_analysis_tools/cython_utils/setup.py
+++ src/oxDNA_analysis_tools/cython_utils/setup.py
@@ -4,5 +4,5 @@ import numpy as np
 
 setup(
     include_dirs=[np.get_include()],
-    ext_modules = cythonize("parse_conf.pyx", annotate=True, compiler_directives={'language_level' : "3"})
+    ext_modules = cythonize("get_confs.pyx", annotate=True, compiler_directives={'language_level' : "3"})
 )
